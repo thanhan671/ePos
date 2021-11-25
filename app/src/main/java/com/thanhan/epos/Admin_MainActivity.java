8a2bf43a4f0a80366e38e124a69ace72f27de06a
vanhung201 <vanhung.work@gmail.com>
2021-11-26 00:32:36 +0700

    HUNG UP CODE CRUD LOAI HANG 26/11/2021

--- app/src/main/java/com/thanhan/epos/Admin_MainActivity.java
+++ app/src/main/java/com/thanhan/epos/Admin_MainActivity.java
@@ -2,13 +2,31 @@ package com.thanhan.epos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Admin_MainActivity extends AppCompatActivity {
+    private Button btnLoaiHang;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_main);
+
+        matching();
+
+        btnLoaiHang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Admin_MainActivity.this, AddProductTypeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void matching() {
+        btnLoaiHang = (Button) findViewById(R.id.hanghoa);
     }
 }
