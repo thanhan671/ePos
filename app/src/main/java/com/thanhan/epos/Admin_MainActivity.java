9dc558401171dd7e0d7d270d4323827b44d830af
thanggiang <83661715+thanggiang@users.noreply.github.com>
2021-12-06 21:33:33 +0700

    login session

--- app/src/main/java/com/thanhan/epos/Admin_MainActivity.java
+++ app/src/main/java/com/thanhan/epos/Admin_MainActivity.java
@@ -6,8 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Admin_MainActivity extends AppCompatActivity {
+    TextView logout;
     private Button btnLoaiHang, btnHangHoa, btnAC;
 
     @Override
@@ -16,6 +19,16 @@ public class Admin_MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_admin_main);
 
         matching();
+        logout = findViewById(R.id.textView2);
+        logout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(Admin_MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+                Toast.makeText(Admin_MainActivity.this, "ĐĂNG XUẤT THÀNH CÔNG", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         btnLoaiHang.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/com/thanhan/epos/LoginActivity.java
@@ -0,0 +1,93 @@
+package com.thanhan.epos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        TextView username =(TextView) findViewById(R.id.username);
+        TextView password =(TextView) findViewById(R.id.password);
+
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+
+        //admin and admin
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("vanhung") && password.getText().toString().equals("ad12345")){
+                    //correct
+                    Toast.makeText(LoginActivity.this,"LOGIN SUCCESSFUL !!! ",Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, Admin_MainActivity.class);
+                    startActivity(intent);
+                }else if (username.getText().toString().equals("thanhan") && password.getText().toString().equals("ad12345")){
+
+                    Toast.makeText(LoginActivity.this, "LOGIN SUCCESSFUL !!!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, Admin_MainActivity.class);
+                    startActivity(intent);
+
+                }else if (username.getText().toString().equals("trinhphu") && password.getText().toString().equals("ad12345")){
+                    Toast.makeText(LoginActivity.this, "LOGIN SUCCESSFUL !!!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, Admin_MainActivity.class);
+                    startActivity(intent);
+
+                }else if (username.getText().toString().equals("thanhan") && password.getText().toString().equals("987654321")){
+
+                    Toast.makeText(LoginActivity.this, "LOGIN SUCCESSFUL !!!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, Staff_MainActivity.class);
+                    startActivity(intent);
+
+                }else if (username.getText().toString().equals("chauly") && password.getText().toString().equals("123456")){
+
+                    Toast.makeText(LoginActivity.this, "LOGIN SUCCESSFUL !!!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, Staff_MainActivity.class);
+                    startActivity(intent);
+
+                }else if (username.getText().toString().equals("vanthang") && password.getText().toString().equals("hhhhh")){
+
+                    Toast.makeText(LoginActivity.this, "LOGIN SUCCESSFUL !!!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, Staff_MainActivity.class);
+                    startActivity(intent);
+
+                }else if (username.getText().toString().equals("thanhhoai") && password.getText().toString().equals("123456")){
+
+                    Toast.makeText(LoginActivity.this, "LOGIN SUCCESSFUL !!!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, Staff_MainActivity.class);
+                    startActivity(intent);
+
+                }
+                else if (username.getText().toString().equals("vanhung") && password.getText().toString().equals("123456")){
+
+                    Toast.makeText(LoginActivity.this, "LOGIN SUCCESSFUL !!!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, Staff_MainActivity.class);
+                    startActivity(intent);
+
+                }else if (username.getText().toString().equals("ngocphu") && password.getText().toString().equals("123456")){
+
+                    Toast.makeText(LoginActivity.this, "LOGIN SUCCESSFUL !!!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, Staff_MainActivity.class);
+                    startActivity(intent);
+
+                }else if (username.getText().toString().equals("thanhtung") && password.getText().toString().equals("123456")){
+
+                    Toast.makeText(LoginActivity.this, "LOGIN SUCCESSFUL !!!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, Staff_MainActivity.class);
+                    startActivity(intent);
+
+                }else Toast.makeText(LoginActivity.this, "LOGIN FAIL !!!", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
